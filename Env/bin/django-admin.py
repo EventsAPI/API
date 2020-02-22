332bf70a268c5c8a48a5c1fed857a72ab240b99c
RicardoWebProject <luisrijhn09@gmail.com>
2020-02-21 20:43:59 -0600

    Django

---
+++ Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/luisr/Documents/Universidad Mariano Gálvez/44. Ingeniería de Software/API/EventsAPI/Env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
