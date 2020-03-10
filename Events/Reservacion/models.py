3e3d55113b3d285127be8f6dae991191ea0c1efc
RicardoWebProject <luisrijhn09@gmail.com>
2020-03-10 00:37:39 -0600

    Modelo de Reservas

--- Events/Reservacion/models.py
+++ Events/Reservacion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Reservas (models.Model):
+    LIBRE = 1
+    RESERVADO = 2
+    
+    ESTADO = (
+        (LIBRE, 'Asiento libre'),
+        (RESERVADO, 'Asiento reservado')
+    )
+    
+    estado = models.SmallIntegerField(choices=ESTADO, default=LIBRE)
+    
+    idUsuario = models.ForeignKey('Usuarios.Usuario', on_delete=models.CASCADE)
+    idLocalidad = models.ForeignKey('Eventos.Eventos', on_delete=models.CASCADE)
+    #El primer 'Eventos' es sobre la app Django; el segundo, sobre el modelo Eventos.
