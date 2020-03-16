c0dec52fe0fc443607fad2f75fdb44535d26dabd
RicardoWebProject <luisrijhn09@gmail.com>
2020-03-15 18:04:49 -0600

    Cambio de basename a name

--- Events/Eventos/urls.py
+++ Events/Eventos/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CrearEvento, ListarEvento, BorrarEvento, ActualizarEvento
 
 urlpatterns = [
-    path('evento/crear', CrearEvento.as_view(), basename = 'crearEvento'),
-    path('evento/mostrar/', ListarEvento.as_view(), basename = 'listarEvento'),
-    path('evento/borrar/', BorrarEvento.as_view(), basename = 'borrarEvento'),
-    path('evento/actualizar/', ActualizarEvento.as_view(), basename = 'actualizarEvento')
+    path('evento/crear', CrearEvento.as_view(), name = 'crearEvento'),
+    path('evento/mostrar/', ListarEvento.as_view(), name = 'listarEvento'),
+    path('evento/borrar/', BorrarEvento.as_view(), name = 'borrarEvento'),
+    path('evento/actualizar/', ActualizarEvento.as_view(), name = 'actualizarEvento')
 ]
