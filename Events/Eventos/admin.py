57053c7f495fb68d48cfbfe67f9b3399110c800c
RicardoWebProject <luisrijhn09@gmail.com>
2020-03-15 18:25:32 -0600

    Registrando modelos en admin

--- Events/Eventos/admin.py
+++ Events/Eventos/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+# Registrando los modelos
+from .models.comentarios import Comentarios
+from .models.eventos import Eventos
+from .models.localidades import Asientos, TipoLocal, Localidad
+from .models.valoraciones import Valoraciones
+
+@admin.register (Comentarios)
+class Comentarios (admin.ModelAdmin):
+    list_display = ('idUsuario', 'comentario', 'fecha',)
+    list_filter = ('fecha', 'idUsuario',)
+
+@admin.register (Eventos)
+class Eventos (admin.ModelAdmin):
+    list_display = ('nombre', 'fecha', 'lugar', 'estado', 'organizadores', 'idComentarios',)
+    list_filter = ('lugar', 'fecha', 'estado', 'organizadores',)
+
+@admin.register (Asientos)
+class Asientos (admin.ModelAdmin):
+    list_display = ('estado',)
+
+@admin.register (TipoLocal)
+class TipoLocal (admin.ModelAdmin):
+    list_display = ('tipo', 'costo',)
+    list_filter = ('tipo', 'costo',)
+
+@admin.register (Localidad)
+class Localidad (admin.ModelAdmin):
+    list_display = ('asientos_totales', 'idAsiento', 'idTipoLocal',)
+
+@admin.register (Valoraciones)
+class Valoraciones (admin.ModelAdmin):
+    list_display = ('valoracion', 'idUsuario',)
--- Events/Usuarios/admin.py
+++ Events/Usuarios/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# Registrando modelos
+from .models.usuarios import Usuario
+from .models.perfiles import Perfil
+
+@admin.register (Perfil)
+class Perfil (admin.ModelAdmin):
+    list_display = ('usuario', 'foto', 'genero', 'biografia',)
+    list_filter = ('usuario', 'genero',)
+
+@admin.register (Usuario)
+class Usuario (admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_verified',)
+    list_filter = ('is_verified',)
