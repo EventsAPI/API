65e67a275fbe3bfb26398060faede79aa7f1b6c5
RicardoWebProject <luisrijhn09@gmail.com>
2020-04-21 18:20:47 -0600

    Cambio en modelo localidades

--- Events/Eventos/models/localidades.py
+++ Events/Eventos/models/localidades.py
@@ -11,21 +11,18 @@ class Asientos (models.Model):
     
     estado = models.SmallIntegerField(choices=ESTADOS, default=LIBRE)
 
-class TipoLocal (models.Model):
+class Localidad (models.Model):
     GENERAL = 1
-    PALCA = 3
+    ORO = 3
     PLATINO = 5
     
     TIPOS = (
         (GENERAL, 'Asiento General'),
-        (PALCA, 'Asiento en Palcos'),
+        (ORO, 'Asiento Oro'),
         (PLATINO, 'Asiento Platinum')
     )
     
     tipo = models.SmallIntegerField(choices=TIPOS, default=GENERAL)
     costo = models.FloatField()
-
-class Localidad (models.Model):
-    asientos_totales = models.IntegerField()
+    
     idAsiento = models.ForeignKey(Asientos,on_delete=models.CASCADE)
-    idTipoLocal = models.ForeignKey(TipoLocal, on_delete=models.CASCADE)
