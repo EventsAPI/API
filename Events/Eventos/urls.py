b995ed0019e0a52923449ee7442239929e3d2d03
RicardoWebProject <luisrijhn09@gmail.com>
2020-04-22 11:29:06 -0600

    agregadas urls

--- Events/Eventos/urls.py
+++ Events/Eventos/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('evento/crear', CrearEvento.as_view(), name = 'crearEvento'),
     path('evento/mostrar/', ListarEvento.as_view(), name = 'listarEvento'),
     path('evento/borrar/<pk>', BorrarEvento.as_view(), name = 'borrarEvento'),
-    path('evento/actualizar/<pk>', ActualizarEvento.as_view(), name = 'actualizarEvento')
+    path('evento/actualizar/<pk>', ActualizarEvento.as_view(), name = 'actualizarEvento'),
+    path('', include(router.urls)),
 ]
