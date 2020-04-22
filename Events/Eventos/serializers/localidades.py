c75650886fdb534211f2a2ef1c6e4d0cf5b8ddfc
RicardoWebProject <luisrijhn09@gmail.com>
2020-04-22 11:24:46 -0600

    cambios localidad serializer

--- Events/Eventos/serializers/localidades.py
+++ Events/Eventos/serializers/localidades.py
@@ -8,20 +8,12 @@ class AsientosSerializer (serializers.ModelSerializer):
 
 
 class LocalidadSerializer (serializers.ModelSerializer):
-    idAsiento = AsientosSerializer (many = True)
+    idAsiento = AsientosSerializer (many=True, read_only=True)
     
     class Meta:
         model = Localidad
-        fields = '__all__'
-        depth = 1
-    
-    def create(self, validated_data):
-        asiento_validated_data = validated_data.pop('idAsiento')
-        localidad = Localidad.objects.create(**validated_data)
-        asiento_set_serializer = self.fields['idAsiento']
-        
-        for each in asiento_validated_data:
-            each['idAsiento'] = localidad
-        asientos = asiento_set_serializer.create(asiento_validated_data)
-        
-        return localidad
+        fields = [
+            'tipo',
+            'costo',
+            'idAsiento'
+        ]
