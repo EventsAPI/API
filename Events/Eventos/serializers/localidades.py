99fc32f31fd854529cda77e1ca3c6e755328fd35
RicardoWebProject <luisrijhn09@gmail.com>
2020-04-21 18:20:57 -0600

    Cambio serializador localidades

--- Events/Eventos/serializers/localidades.py
+++ Events/Eventos/serializers/localidades.py
@@ -1,19 +1,14 @@
 from rest_framework import serializers
-from Eventos.models.localidades import Localidad, Asientos, TipoLocal
+from ..models.localidades import Localidad, Asientos
 
 class AsientosSerializer (serializers.ModelSerializer):
     class Meta:
         model = Asientos
         fields = '__all__'
 
-class TipoLocalSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = TipoLocal
-        fields = '__all__'
 
 class LocalidadSerializer (serializers.ModelSerializer):
     asientos = AsientosSerializer (many = True , read_only = True)
-    tipos = TipoLocalSerializer (many = True, read_only = True)
     
     class Meta:
         model = Localidad
