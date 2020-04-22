55becb8d5fc8e9986fc03a7f0b8e2cbb7af0d653
RicardoWebProject <luisrijhn09@gmail.com>
2020-04-21 20:57:23 -0600

    Actualizado serializador para localidades

--- Events/Eventos/serializers/localidades.py
+++ Events/Eventos/serializers/localidades.py
@@ -8,13 +8,20 @@ class AsientosSerializer (serializers.ModelSerializer):
 
 
 class LocalidadSerializer (serializers.ModelSerializer):
-    asientos = AsientosSerializer (many = True , read_only = True)
+    idAsiento = AsientosSerializer (many = True)
     
     class Meta:
         model = Localidad
-        fields = [
-            'asientos_totales',
-            'asientos',
-            'tipos'
-        ]
+        fields = '__all__'
         depth = 1
+    
+    def create(self, validated_data):
+        asiento_validated_data = validated_data.pop('idAsiento')
+        localidad = Localidad.objects.create(**validated_data)
+        asiento_set_serializer = self.fields['idAsiento']
+        
+        for each in asiento_validated_data:
+            each['idAsiento'] = localidad
+        asientos = asiento_set_serializer.create(asiento_validated_data)
+        
+        return localidad
