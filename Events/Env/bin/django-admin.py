4593eb52a7c0886e5d2a9f29f0d99b7752b1d044
javiermiranda <pablomir4nda@gmail.com>
2020-05-10 19:46:10 -0500

    listo

--- Events/Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/Javier Miranda/Desktop/unvirsidad/9no semestre/pago/API/Env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
