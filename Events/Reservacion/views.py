8f9f0c8e6ac05212abf8c83199d51f3769cf8861
lupis97 <guadalupevasquez201443@gmail.com>
2020-05-11 22:49:05 -0600

    listo

--- Events/Reservacion/views.py
+++ Events/Reservacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .Serializers import Reservacion
 from rest_framework import viewsets 
-from .models import Reservacion
+from .models import Reserva
 
 class ReservacionViewSet(viewsets.ModelViewSet):
     queryset = Reservacion.objects.all()
