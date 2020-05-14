9df992c4dc5eaaa101aa08d1f5a397e5c9c91fea
RicardoWebProject <luisrijhn09@gmail.com>
2020-05-14 17:51:16 -0600

    Agregado método create para enviar email

--- Events/Eventos/views/eventos.py
+++ Events/Eventos/views/eventos.py
@@ -4,13 +4,33 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
 #Serializador
 from ..serializers.eventos import EventosSerializer
-#Modelo
+#Modelos
 from ..models.eventos import Eventos
+from Usuarios.models.usuarios import Usuario
+#Email
+from django.core.mail import send_mail
+from django.conf import settings
 
 class CrearEvento (CreateAPIView):
     queryset = Eventos.objects.all()
     serializer_class = EventosSerializer
     permission_classes = []
+    
+    #Envío de Email al crear evento
+    
+    def create(self, request, *args, **kwargs):
+        usuario = Usuario.objects.all()
+        respuesta = super(CrearEvento, self).create(request, *args, **kwargs)
+        
+        subject = '¡Nuevos eventos acercándose!'
+        message = 'Tenemos nuevos eventos a la espera. Cuando puedas, ingresa a nuestra página.'
+        from_email = 'API Events <noreply {}>'.format(settings.EMAIL_HOST_USER)
+        
+        for usuarios in usuario:
+            to_list = [usuarios.email]
+            send_mail(subject, message, from_email, to_list, fail_silently=True)
+            
+        return respuesta
 
 class ListarEvento (ListAPIView):
     queryset = Eventos.objects.all()
